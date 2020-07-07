34ec5441cb59e821c7a8e8868301688227c234f6
Grupo1 <c-24-c-grupo1@hotmail.com>
2020-07-07 12:58:42 +0000

    Creación inicial del proyecto y configuracion con MongoDB

---
+++ app/Servicio.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+//use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Servicio extends Model
+{
+    protected $fillale=[
+        'servicio_nombre','servicio_descripcion',
+        'servicio_precio','cat_id',
+    ];
+}
